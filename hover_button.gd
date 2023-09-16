0ea819dea673a8ae6fa3d6d8d11d89bb9161bede
Stevnmc32 <18pucercowboy@gmail.com>
2023-09-16 11:51:54 +0200

    basic ui stuff

---
+++ hover_button.gd
@@ -0,0 +1,46 @@
+extends Node2D
+
+var is_disabled = false
+signal button_pressed
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Text.text=get_meta("text");
+	return;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	return;
+
+
+func _on_button_2_mouse_entered():
+	if is_disabled:
+		return;
+	$Sprite.show();
+	return;
+
+
+func _on_button_2_mouse_exited():
+	if is_disabled:
+		return;
+	$Sprite.hide();
+	return;
+
+
+func _on_button_2_pressed():
+	button_pressed.emit();
+	return;
+
+func enable():
+	is_disabled=false;
+	$Button.disabled=false;
+	$Button.mouse_default_cursor_shape=Button.CURSOR_POINTING_HAND;
+	return;
+	
+func disable():
+	is_disabled=true;
+	$Button.disabled=true;
+	$Button.mouse_default_cursor_shape=Button.CURSOR_ARROW;
+	$Sprite.hide();
+	return;
+
